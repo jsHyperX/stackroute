ef5883c373c95d06e7ea3693d7679edcc5cbba7e
jsHyperX <vikramsaurabh14@gmaiil.com>
2018-08-24 13:09:26 +0530

    added javascript file

---
+++ script.js
@@ -0,0 +1,9 @@
+function redirect2home() {
+    window.location = "home.html";
+}
+function redirect2about() {
+    window.location = "about.html";
+}
+function redirect2contact() {
+    window.location = "contact.html";
+}
